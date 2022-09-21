8950dacd6cd2eb7552bf54676d22613636dfed1e
ecoliere <113115093+ecoliere@users.noreply.github.com>
2022-09-21 21:22:34 +0300

    Update zadanie2.3.c

--- zadanie2.3.c
+++ zadanie2.3.c
@@ -4,5 +4,5 @@ void main(void)
 {
     setlocale(LC_ALL, "RUS");
     int n = 4;
-    printf("Дано:\n\t 4 \n\t 333 \n\t        \nОтвет:\n\t%5.3f", n/333.);
+    printf("Р”Р°РЅРѕ:\n\t 4 \n\t 333 \n\t        \nРћС‚РІРµС‚:\n\t%5.3f", n/333.);
 }
